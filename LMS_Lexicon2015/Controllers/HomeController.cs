1fec0911394a916e9226867a27f8e0d5433a3aa2
chrikro <christina@chrikro.net>
2015-12-08 16:40:48 +0100

    Temo bortmarkering homecontroller

--- LMS_Lexicon2015/Controllers/HomeController.cs
+++ LMS_Lexicon2015/Controllers/HomeController.cs
@@ -15,11 +15,13 @@ namespace LMS_Lexicon2015.Controllers
         public ActionResult Index()
         {
 
-            
-            var groupId =  new LMS_Lexicon2015.Models.ApplicationDbContext().Users.Where(u => u.Email == User.Identity.Name).FirstOrDefault().GroupId;
-            ViewBag.groupId = groupId;
-            ViewBag.courseOccasionId = new LMS_Lexicon2015.Models.ApplicationDbContext().CourseOccasions.Where(o => o.GroupId == (int)groupId && o.StartDate < DateTime.Today && o.EndDate > DateTime.Today).First().Activities.First().CourseId;
-  
+        if (Request.IsAuthenticated)
+            {
+            //lagt till request.isauth.. ovan samt bortmerkerat raderna inom klamrar nedan. Temporärt så att det fungera onsdag morgon. CK
+                //var groupId = new LMS_Lexicon2015.Models.ApplicationDbContext().Users.Where(u => u.Email == User.Identity.Name).FirstOrDefault().GroupId;
+                //ViewBag.groupId = groupId;
+                //ViewBag.courseOccasionId = new LMS_Lexicon2015.Models.ApplicationDbContext().CourseOccasions.Where(o => o.GroupId == (int)groupId && o.StartDate < DateTime.Today && o.EndDate > DateTime.Today).First().Activities.First().CourseId;
+            }
             HomeIndex = true;
             return View();
         }
