2c8be906b072db68eea8c58bee2a388a58d9df9e
chrikro <christina@chrikro.net>
2015-12-14 13:58:32 +0100

    bootstrapfix

--- LMS_Lexicon2015/Controllers/HomeController.cs
+++ LMS_Lexicon2015/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace LMS_Lexicon2015.Controllers
                 //var groupId = new LMS_Lexicon2015.Models.ApplicationDbContext().Users.Where(u => u.Email == User.Identity.Name).FirstOrDefault().GroupId;
                 //ViewBag.groupId = groupId;
                 //ViewBag.courseOccasionId = new LMS_Lexicon2015.Models.ApplicationDbContext().CourseOccasions.Where(o => o.GroupId == (int)groupId && o.StartDate < DateTime.Today && o.EndDate > DateTime.Today).First().Activities.First().CourseId;
+
             }
             HomeIndex = true;
             return View();
