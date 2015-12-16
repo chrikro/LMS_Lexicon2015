7936f6fa14c16e19e643011f7d164c14e585602f
chrikro <christina@chrikro.net>
2015-12-16 11:12:08 +0100

    rättat_seed_niswan@ är tillbaka och fältet deadline för doc har lagts till seeden

--- LMS_Lexicon2015/Migrations/Configuration.cs
+++ LMS_Lexicon2015/Migrations/Configuration.cs
@@ -26,62 +26,63 @@ namespace LMS_Lexicon2015.Migrations
                     var role = new IdentityRole { Name = roles };
                     roleManager.Create(role);
                 }
-            }
-            /////----------------------
 
-            //Aktivitets typ // lägg till activityTypes 
-            var activityTypes = new[] {
+            }
+                /////----------------------
+
+                //Aktivitets typ // lägg till activityTypes 
+                var activityTypes = new[] {
                 new ActivityType { Name = "Föreläsning" },
                 new ActivityType { Name = "E-learning" },
                 new ActivityType { Name = "ÖvningsUppgift" },
                 new ActivityType { Name = "Projekt" }
            };
 
-            context.ActivityTypes.AddOrUpdate(at => at.Name, activityTypes);
-            context.SaveChanges();
-            /////----------------------
-            //grupper
-            var groups = new[] {
+                context.ActivityTypes.AddOrUpdate(at => at.Name, activityTypes);
+                context.SaveChanges();
+                /////----------------------
+                //grupper
+                var groups = new[] {
                 new Group { Name = ".net Mar 2015", Description = "dotNet-utbildning. Påbyggnad våren-sommaren 2015.", StartDate = new DateTime(2015,03,06,09,00,00), EndDate = new DateTime(2015,08,21,17,00,00)  },    
                 new Group { Name = ".net Sep 2015", Description = "dotNet-utbildning. Påbyggnad hösten 2015.", StartDate = new DateTime(2015, 08, 31,09,00,00), EndDate = new DateTime(2015, 12, 20,17,00,00) },
                 new Group { Name = "Java Sep 2015", Description = "Java-utbildning.", StartDate = new DateTime(2015, 08, 31,09,00,00), EndDate = new DateTime(2015, 12, 20,17,00,00) },
                 new Group { Name = ".net Feb 2016", Description = "dotNet-utbildning. Påbyggnad våren 2016.", StartDate = new DateTime(2016, 02, 28,09,00,00), EndDate = new DateTime(2016, 06, 16,17,00,00) }
             };
 
-            context.Groups.AddOrUpdate(g => g.Name, groups);
-            context.SaveChanges();
-            /////-------------------------------------------------------------------------------------------
-            // Användare (med hash lösenord om det inte finns) alla har lösenord=hej999
+                context.Groups.AddOrUpdate(g => g.Name, groups);
+                context.SaveChanges();
+                /////-------------------------------------------------------------------------------------------
+                // Användare (med hash lösenord om det inte finns) alla har lösenord=hej999
 
-            var store = new UserStore<ApplicationUser>(context);
-            var UserManager = new UserManager<ApplicationUser>(store);
+                var store = new UserStore<ApplicationUser>(context);
+                var UserManager = new UserManager<ApplicationUser>(store);
 
-            //bäst testdata för kalle anka (niswa..) och jultomten, grupp[1] (net sep 2015), sql-kursen
+                //bäst testdata för kalle anka (niswa..) och jultomten, grupp[1] (net sep 2015), sql-kursen
 
-            createNewUser(context, groups[0].Id, UserManager, "Christina", "Kaffekopp", "Elev", "chrikro@gmail.com");
-            createNewUser(context, groups[0].Id, UserManager, "Lucia", "da Roma", "Lärare", "lucia@roma.it"); 
-            createNewUser(context, groups[1].Id, UserManager, "Kalle", "Anka", "Lärare", "kalle.anka@hotmail.com");
-            createNewUser(context, groups[1].Id, UserManager, "Anna-Karin", "Rönnegård", "Lärare", "a.ronnegard@gmail.com");
-            createNewUser(context, groups[1].Id, UserManager, "Tomte", "von Nordpoolen", "Elev", "jultomten@nordpoolen.org");
-            createNewUser(context, groups[1].Id, UserManager, "Leo", "Henning", "Elev", "leo.henning@dfdf.se");
-            createNewUser(context, groups[1].Id, UserManager, "Oscar", "Gustavsson", "Elev", "oscar.gustavssong@dfdf.se");
-            createNewUser(context, groups[1].Id, UserManager, "Lilly", "Bergqvist", "Elev", "lilly@ander.se");
-            createNewUser(context, groups[1].Id, UserManager, "Ingvar", "Andersson", "Elev", "ingvar.andersson@hotmail.com");
-            createNewUser(context, groups[2].Id, UserManager, "Kalle", "Andersson", "Elev", "Kalle@Kalle.se");
-            createNewUser(context, groups[2].Id, UserManager, "Liam", "Ahlqvist", "Elev", "liam.a@fff.se");
-            createNewUser(context, groups[2].Id, UserManager, "Donald", "Trump", "Elev", "donald.trump@muppetshow.com");
-            createNewUser(context, groups[2].Id, UserManager, "Angela", "Merkel", "Lärare", "angela.merkel@yahoo.de");
-            createNewUser(context, groups[2].Id, UserManager, "Kalle", "Lundvall", "Elev", "kalle.lundvall@hotmail.com");
-            createNewUser(context, groups[2].Id, UserManager, "Alfred", "Lejon", "Lärare", "alfred.lejon@ff.se");
-            createNewUser(context, groups[3].Id, UserManager, "Wilma", "Benjaminsson", "Lärare", "wilma.b@hotmail.com");
-            createNewUser(context, groups[3].Id, UserManager, "Elsa", "Andersson", "Elev", "elsa.andersson@hej.se");
+                createNewUser(context, groups[0].Id, UserManager, "Christina", "Kaffekopp", "Elev", "chrikro129@gmail.com");
+                createNewUser(context, groups[0].Id, UserManager, "Lucia", "da Roma", "Lärare", "lucia@roma.it");
+                createNewUser(context, groups[1].Id, UserManager, "Nisse", "Hult", "Lärare", "nisaw99@hotmail.com");
+                createNewUser(context, groups[1].Id, UserManager, "Anna-Karin", "Rönnegård", "Lärare", "a.ronnegard@gmail.com");
+                createNewUser(context, groups[1].Id, UserManager, "Tomte", "von Nordpoolen", "Elev", "jultomten@nordpoolen.org");
+                createNewUser(context, groups[1].Id, UserManager, "Leo", "Henning", "Elev", "leo.henning@dfdf.se");
+                createNewUser(context, groups[1].Id, UserManager, "Oscar", "Gustavsson", "Elev", "oscar.gustavssong@dfdf.se");
+                createNewUser(context, groups[1].Id, UserManager, "Lilly", "Bergqvist", "Elev", "lilly@ander.se");
+                createNewUser(context, groups[1].Id, UserManager, "Ingvar", "Andersson", "Elev", "ingvar.andersson@hotmail.com");
+                createNewUser(context, groups[2].Id, UserManager, "Kalle", "Andersson", "Elev", "Kalle@Kalle.se");
+                createNewUser(context, groups[2].Id, UserManager, "Liam", "Ahlqvist", "Elev", "liam.a@fff.se");
+                createNewUser(context, groups[2].Id, UserManager, "Donald", "Trump", "Elev", "donald.trump@muppetshow.com");
+                createNewUser(context, groups[2].Id, UserManager, "Angela", "Merkel", "Lärare", "angela.merkel@yahoo.de");
+                createNewUser(context, groups[2].Id, UserManager, "Kalle", "Lundvall", "Elev", "kalle.lundvall@hotmail.com");
+                createNewUser(context, groups[2].Id, UserManager, "Alfred", "Lejon", "Lärare", "alfred.lejon@ff.se");
+                createNewUser(context, groups[3].Id, UserManager, "Wilma", "Benjaminsson", "Lärare", "wilma.b@hotmail.com");
+                createNewUser(context, groups[3].Id, UserManager, "Elsa", "Andersson", "Elev", "elsa.andersson@hej.se");
 
-            context.SaveChanges();
+                context.SaveChanges();
 
-            /////----------------------
-            //kurser 
-            // flest kurser till grupp[1] (.NET sep 2015)
-            var courses = new[] {
+                /////----------------------
+                //kurser 
+                // flest kurser till grupp[1] (.NET sep 2015)
+                var courses = new[] {
                 new CourseOccasion { Name = "csharp", Description = "Text text text text", StartDate = new DateTime(2015, 03, 11,09,00,00), EndDate = new DateTime(2015, 03, 16,16,30,00), GroupId = groups[0].Id  },
                 new CourseOccasion { Name = "Angular JS", Description = "Text text text text", StartDate = new DateTime(2015, 04, 27,09,00,00), EndDate = new DateTime(2015, 05, 10,16,30,00), GroupId = groups[0].Id  },
                 new CourseOccasion { Name = "SQL", Description = "Språk för att manipulera data i en databas.", StartDate = new DateTime(2015, 08, 31,09,00,00), EndDate = new DateTime(2015, 09, 05,16,30,00), GroupId = groups[1].Id  },
@@ -93,14 +94,14 @@ namespace LMS_Lexicon2015.Migrations
 
             };
 
-            context.CourseOccasions.AddOrUpdate(co => co.Name, courses);
-            context.SaveChanges();
+                context.CourseOccasions.AddOrUpdate(co => co.Name, courses);
+                context.SaveChanges();
 
-            /////----------------------
-            //Aktiviteter 
-            //obs end i seeden. Olika End-datum
-            //det finns bara aktiviteter till kurser som ingår i grupp[1].
-            var activitys = new[] {
+                /////----------------------
+                //Aktiviteter 
+                //obs end i seeden. Olika End-datum
+                //det finns bara aktiviteter till kurser som ingår i grupp[1].
+                var activitys = new[] {
                 new Activity{ Name = activityTypes[0].Name, Description = "Vad är SQL? Intro", StartDate = new DateTime(2015,09,01,09,00,00), EndDate = new DateTime(2015,09,01,15,30,00), CourseId = courses[2].Id },
                 new Activity{ Name = activityTypes[1].Name, Description = "För att öva sql.", StartDate = new DateTime(2015,09,02,09,00,00), EndDate = new DateTime(2016,09,03,15,30,00), CourseId = courses[2].Id },
                 new Activity{ Name = activityTypes[2].Name, Description = "Om artefakter och samarbete", StartDate = new DateTime(2015,09,06,09,00,00), EndDate = new DateTime(2015,09,06,15,30,00), CourseId = courses[3].Id },
@@ -108,24 +109,26 @@ namespace LMS_Lexicon2015.Migrations
                 new Activity{ Name = activityTypes[0].Name, Description = "Testteknik lärs ut. Även andra aspekter av test tas upp under den här aktiviteten.", StartDate = new DateTime(2015,09,20,09,00,00), EndDate = new DateTime(2015,09,22,15,30,00), CourseId = courses[4].Id }
            };
 
-            context.Activitys.AddOrUpdate(at => at.EndDate, activitys);
-            context.SaveChanges();
+                context.Activitys.AddOrUpdate(at => at.EndDate, activitys);
+                context.SaveChanges();
 
-            /////----------------------
-            //Documents (document måste finnas i identitymodel för att seed av dok ska fungera.
-            
-            createNewDocument(context, groups[1].Id,null,null, "nisaw99@hotmail.com", "ReadMe.txt", "Ett nytt fint dokument som handlar om något.", new DateTime(2015, 09, 01,10,30,00));
-            createNewDocument(context, groups[1].Id, courses[2].Id, null, "jultomten@nordpoolen.org", "csharp_extra.txt", "Ett nytt fint dokument som handlar om C#.", new DateTime(2015, 09, 02,14,00,00));
-            createNewDocument(context, groups[1].Id, courses[2].Id, activitys[0].Id, "jultomten@nordpoolen.org", "test.txt", "Ett nytt fint dokument som handlar om SQL.", new DateTime(2015, 09, 03,09,30,00));
-        }
+                /////----------------------
+                //Documents (document måste finnas i identitymodel för att seed av dok ska fungera.
 
-        // ------------- separat metod för att skapa nya dokument. Anropas fr ovan -----------------------
-        private static void createNewDocument(LMS_Lexicon2015.Models.ApplicationDbContext context, int? groupIdNewDoc, int? courseIdNewDoc, int? activitieIdNewDoc, string docOwnerNew, string docNamnNew, string docDescriptNew, DateTime docTimestampNew)
+                createNewDocument(context, groups[1].Id, null, null, "nisaw99@hotmail.com", "ReadMe.txt", "Ett nytt fint dokument som handlar om något.", new DateTime(2015, 09, 01, 10, 30, 00), new DateTime(2015, 09, 02, 10, 30, 00));
+                createNewDocument(context, groups[1].Id, courses[2].Id, null, "jultomten@nordpoolen.org", "csharp_extra.txt", "Ett nytt fint dokument som handlar om C#.", new DateTime(2015, 09, 02, 14, 00, 00), new DateTime(2015, 09, 02, 17, 00, 00));
+                createNewDocument(context, groups[1].Id, courses[2].Id, activitys[0].Id, "jultomten@nordpoolen.org", "test.txt", "Ett nytt fint dokument som handlar om SQL.", new DateTime(2015, 09, 03, 09, 30, 00), null);
+            }
+        
+        // ------------- separat metod för att skapa nya dokument. Anropas fr ovan 
+        //  i seeden sätts namn och url till samma sak, dvs filnamn. //-----------------------
+        private static void createNewDocument(LMS_Lexicon2015.Models.ApplicationDbContext context, int? groupIdNewDoc, int? courseIdNewDoc, int? activitieIdNewDoc, string docOwnerNew, string docNamnNew, string docDescriptNew, DateTime docTimestampNew, DateTime? docDeadLineNew)
         {
             var documents = new[] {
                     new Document { Name = docNamnNew, 
                     Url = docNamnNew, 
                     Description = docDescriptNew, 
+                    Deadline = docDeadLineNew,
                     Timestamp = docTimestampNew, 
                     GroupId = groupIdNewDoc , 
                     UserId = context.Users.Where(u => u.UserName == docOwnerNew).FirstOrDefault().Id ,  
