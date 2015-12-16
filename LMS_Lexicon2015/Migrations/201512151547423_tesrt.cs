67641f01b9c506e9f4a7c356cb5b6945be5c3b82
AnnaRonnegard <a.ronnegard@gmail.com>
2015-12-16 11:23:57 +0100

    nada

---
+++ LMS_Lexicon2015/Migrations/201512151547423_tesrt.cs
@@ -0,0 +1,16 @@
+namespace LMS_Lexicon2015.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class tesrt : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
